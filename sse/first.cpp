52cd27a460f3fb3ddd349caa59c1fef976a88233
Ivaylo Tsankov <zub40000@gmail.com>
2015-11-17 09:10:33 +0200

    * Add omp clock

--- sse/first.cpp
+++ sse/first.cpp
@@ -1,9 +1,12 @@
 #include <immintrin.h>
 #include <iostream>
-
+#include <omp.h>
 
 int main()
 {
+    double start = omp_get_wtime();
     std::cout << "Hello HPC" << std::endl;
+    double end = omp_get_wtime();
+    std::cout << "execution time: " << end - start << std::endl;
     return (0);
 }
